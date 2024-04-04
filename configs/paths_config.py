701e4d67a5f0eceb1471a21b05a8129c7bdf7d08
Federico6419 <102321945+Federico6419@users.noreply.github.com>
2024-04-04 12:07:36 +0200

    Update paths_config.py

--- configs/paths_config.py
+++ configs/paths_config.py
@@ -9,7 +9,7 @@ DATASET_PATHS = {
 MODEL_PATHS = {
 	'AFHQ': "pretrained/afhq_dog_4m.pt",
 	'FFHQ': "pretrained/ffhq_10m.pt",
-	'ir_se50': '../../../drive/MyDrive/model_ir_se50.pth',
+	'ir_se50': '../../../drive/MyDrive/CLIPDiffusion/model_ir_se50.pth',
     'IMAGENET': "pretrained/512x512_diffusion.pt",
 	'shape_predictor': "pretrained/shape_predictor_68_face_landmarks.dat.bz2",
 }

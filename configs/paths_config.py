328a9782babe8aeeb384e50b1bfccca74afccd99
Federico6419 <102321945+Federico6419@users.noreply.github.com>
2024-04-22 13:40:24 +0200

    Update paths_config.py

--- configs/paths_config.py
+++ configs/paths_config.py
@@ -15,3 +15,12 @@ MODEL_PATHS = {
     'IMAGENET': "pretrained/512x512_diffusion.pt",
 	'shape_predictor': "pretrained/shape_predictor_68_face_landmarks.dat.bz2",
 }
+
+HYBRID_MODEL_PATHS = [
+	'./checkpoint/human_face/curly_hair_t401.pth',
+	'./checkpoint/human_face/with_makeup_t401.pth',
+]
+
+HYBRID_CONFIG = \
+	{ 300: [0.4, 0.6, 0],
+	    0: [0.15, 0.15, 0.7]}

d2fa71cb83a4126909e03522772f2ef60d334d8a
Federico6419 <102321945+Federico6419@users.noreply.github.com>
2024-04-22 13:01:50 +0200

    Update paths_config.py

--- configs/paths_config.py
+++ configs/paths_config.py
@@ -5,22 +5,13 @@ DATASET_PATHS = {
 	'LSUN':  'data/lsun',
     'IMAGENET': 'data/imagenet/',
     'retinal': 'data/retinal/',
+    'chest': 'data/chest/'
 }
 
 MODEL_PATHS = {
-	'AFHQ': "pretrained/afhq_dog_4m.pt",
+	'AFHQ': "../../../drive/MyDrive/CLIPDiffusion/afhq_dog_4m.pt",
 	'FFHQ': "pretrained/ffhq_10m.pt",
 	'ir_se50': '../../../drive/MyDrive/CLIPDiffusion/model_ir_se50.pth',
     'IMAGENET': "pretrained/512x512_diffusion.pt",
 	'shape_predictor': "pretrained/shape_predictor_68_face_landmarks.dat.bz2",
 }
-
-
-HYBRID_MODEL_PATHS = [
-	'./checkpoint/human_face/curly_hair_t401.pth',
-	'./checkpoint/human_face/with_makeup_t401.pth',
-]
-
-HYBRID_CONFIG = \
-	{ 300: [0.4, 0.6, 0],
-	    0: [0.15, 0.15, 0.7]}
